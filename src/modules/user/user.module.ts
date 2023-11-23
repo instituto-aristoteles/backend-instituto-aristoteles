024aed664cda7c3f728e95a0cb833b946863b7bc
Marcos Vinícius <marcosvinicius.analucia@gmail.com>
2023-11-23 09:08:06 -0300

    refatoração de envio de email para trabalhar com eventos de criação de usuário

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,11 +4,12 @@ import { UserController } from './api/user.controller';
 import { UserRepository } from '@/modules/user/repositories/user.repository.impl';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '@/domain/entities/user.entity';
+import { UserCreatedListener } from '@/domain/listeners/user/user-created.listener';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, UserCreatedListener],
   exports: [UserService],
 })
 export class UserModule {}
